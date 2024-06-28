c6f09fbdfe690e17ee74d796980648659666ac49
Krishna sai <117358318+krishnasainakka@users.noreply.github.com>
2024-06-28 23:35:25 +0530

    added credits functionality for users

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -33,7 +33,7 @@ import { updateCredits } from "@/lib/actions/user.actions"
 import { getCldImageUrl } from "next-cloudinary"
 import { addImage, updateImage } from "@/lib/actions/image.actions"
 import { useRouter } from "next/navigation"
-// import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
  
 export const formSchema = z.object({
   title: z.string(),
@@ -173,9 +173,9 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
 
     setNewTransformation(null)
 
-    // startTransition(async () => {
-    //   await updateCredits(userId, creditFee)
-    // })
+    startTransition(async () => {
+      await updateCredits(userId, creditFee)
+    })
   }
 
   // useEffect(() => {
@@ -188,7 +188,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} 
         className="space-y-8">
-        {/* {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />} */}
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField 
           control={form.control}
           name="title"
